22801fc993a1f30d4f381dc30f42a8e1d247f765
Gabe <gabeshin.ts@gmail.com>
2018-08-24 11:55:48 +0900

    Home Credit Default Risk Feature Engineering / Param Tuning added.

--- 201808 - Home Credit Default Risk/utils.py
+++ 201808 - Home Credit Default Risk/utils.py
@@ -1,5 +1,9 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import sys
 
 # Function to calculate missing values by column# Funct
 def missing_values_table(df):
@@ -28,3 +32,253 @@ def missing_values_table(df):
 
         # Return the dataframe with missing information
         return mis_val_table_ren_columns
+
+def remove_missing_columns(train, test, threshold = 90):
+    # Calculate missing stats for train and test (remember to calculate a percent!)
+    train_miss = pd.DataFrame(train.isnull().sum())
+    train_miss['percent'] = 100 * train_miss[0] / len(train)
+
+    test_miss = pd.DataFrame(test.isnull().sum())
+    test_miss['percent'] = 100 * test_miss[0] / len(test)
+
+    # list of missing columns for train and test
+    missing_train_columns = list(train_miss.index[train_miss['percent'] > threshold])
+    missing_test_columns = list(test_miss.index[test_miss['percent'] > threshold])
+
+    # Combine the two lists together
+    missing_columns = list(set(missing_train_columns + missing_test_columns))
+
+    # Print information
+    print('There are %d columns with greater than %d%% missing values.' % (len(missing_columns), threshold))
+
+    # Drop the missing columns and return
+    train = train.drop(columns = missing_columns)
+    test = test.drop(columns = missing_columns)
+
+    return train, test
+
+def return_size(df):
+    """Return size of dataframe in gigabytes"""
+    return round(sys.getsizeof(df) / 1e9, 2)
+
+def convert_types(df, print_info = False):
+
+    original_memory = df.memory_usage().sum()
+
+    # Iterate through each column
+    for c in df:
+
+        # Convert ids and booleans to integers
+        if ('SK_ID' in c):
+            df[c] = df[c].fillna(0).astype(np.int32)
+
+        # Convert objects to category
+        elif (df[c].dtype == 'object') and (df[c].nunique() < df.shape[0]):
+            df[c] = df[c].astype('category')
+
+        # Booleans mapped to integers
+        elif list(df[c].unique()) == [1, 0]:
+            df[c] = df[c].astype(bool)
+
+        # Float64 to float32
+        elif df[c].dtype == float:
+            df[c] = df[c].astype(np.float32)
+
+        # Int64 to int32
+        elif df[c].dtype == int:
+            df[c] = df[c].astype(np.int32)
+
+    new_memory = df.memory_usage().sum()
+
+    if print_info:
+        print(f'Original Memory Usage: {round(original_memory / 1e9, 2)} gb.')
+        print(f'New Memory Usage: {round(new_memory / 1e9, 2)} gb.')
+
+    return df
+
+def count_categorical(df, group_var, df_name):
+    """Computes counts and normalized counts for each observation
+    of `group_var` of each unique category in every categorical variable
+
+    Parameters
+    --------
+    df : dataframe
+        The dataframe to calculate the value counts for.
+
+    group_var : string
+        The variable by which to group the dataframe. For each unique
+        value of this variable, the final dataframe will have one row
+
+    df_name : string
+        Variable added to the front of column names to keep track of columns
+
+
+    Return
+    --------
+    categorical : dataframe
+        A dataframe with counts and normalized counts of each unique category in every categorical variable
+        with one row for every unique value of the `group_var`.
+
+    """
+
+    # Select the categorical columns
+    categorical = pd.get_dummies(df.select_dtypes('object'))
+
+    # Make sure to put the identifying id on the column
+    categorical[group_var] = df[group_var]
+
+    # Groupby the group var and calculate the sum and mean
+    categorical = categorical.groupby(group_var).agg(['sum', 'mean'])
+
+    column_names = []
+
+    # Iterate through the columns in level 0
+    for var in categorical.columns.levels[0]:
+        # Iterate through the stats in level 1
+        for stat in ['count', 'count_norm']:
+            # Make a new column name
+            column_names.append('%s_%s_%s' % (df_name, var, stat))
+
+    categorical.columns = column_names
+
+    return categorical
+
+
+def agg_numeric(df, parent_var, df_name):
+    """
+    Groups and aggregates the numeric values in a child dataframe
+    by the parent variable.
+
+    Parameters
+    --------
+        df (dataframe):
+            the child dataframe to calculate the statistics on
+        parent_var (string):
+            the parent variable used for grouping and aggregating
+        df_name (string):
+            the variable used to rename the columns
+
+    Return
+    --------
+        agg (dataframe):
+            a dataframe with the statistics aggregated by the `parent_var` for
+            all numeric columns. Each observation of the parent variable will have
+            one row in the dataframe with the parent variable as the index.
+            The columns are also renamed using the `df_name`. Columns with all duplicate
+            values are removed.
+
+    """
+
+    # Remove id variables other than grouping variable
+    for col in df:
+        if col != parent_var and 'SK_ID' in col:
+            df = df.drop(columns = col)
+
+    # Only want the numeric variables
+    parent_ids = df[parent_var].copy()
+    numeric_df = df.select_dtypes('number').copy()
+    numeric_df[parent_var] = parent_ids
+
+    # Group by the specified variable and calculate the statistics
+    agg = numeric_df.groupby(parent_var).agg(['count', 'mean', 'max', 'min', 'sum'])
+
+    # Need to create new column names
+    columns = []
+
+    # Iterate through the variables names
+    for var in agg.columns.levels[0]:
+        if var != parent_var:
+            # Iterate through the stat names
+            for stat in agg.columns.levels[1]:
+                # Make a new column name for the variable and stat
+                columns.append('%s_%s_%s' % (df_name, var, stat))
+
+    agg.columns = columns
+
+    # Remove the columns with all redundant values
+    _, idx = np.unique(agg, axis = 1, return_index=True)
+    agg = agg.iloc[:, idx]
+
+    return agg
+
+def remove_missing_columns(train, test, threshold = 90):
+    # Calculate missing stats for train and test (remember to calculate a percent!)
+    train_miss = pd.DataFrame(train.isnull().sum())
+    train_miss['percent'] = 100 * train_miss[0] / len(train)
+
+    test_miss = pd.DataFrame(test.isnull().sum())
+    test_miss['percent'] = 100 * test_miss[0] / len(test)
+
+    # list of missing columns for train and test
+    missing_train_columns = list(train_miss.index[train_miss['percent'] > threshold])
+    missing_test_columns = list(test_miss.index[test_miss['percent'] > threshold])
+
+    # Combine the two lists together
+    missing_columns = list(set(missing_train_columns + missing_test_columns))
+
+    # Print information
+    print('There are %d columns with greater than %d%% missing values.' % (len(missing_columns), threshold))
+
+    # Drop the missing columns and return
+    train = train.drop(columns = missing_columns)
+    test = test.drop(columns = missing_columns)
+
+    return train, test
+
+def plot_feature_importances(df, threshold = 0.9):
+    """
+    Plots 15 most important features and the cumulative importance of features.
+    Prints the number of features needed to reach threshold cumulative importance.
+
+    Parameters
+    --------
+    df : dataframe
+        Dataframe of feature importances. Columns must be feature and importance
+    threshold : float, default = 0.9
+        Threshold for prining information about cumulative importances
+
+    Return
+    --------
+    df : dataframe
+        Dataframe ordered by feature importances with a normalized column (sums to 1)
+        and a cumulative importance column
+
+    """
+
+    plt.rcParams['font.size'] = 18
+
+    # Sort features according to importance
+    df = df.sort_values('importance', ascending = False).reset_index()
+
+    # Normalize the feature importances to add up to one
+    df['importance_normalized'] = df['importance'] / df['importance'].sum()
+    df['cumulative_importance'] = np.cumsum(df['importance_normalized'])
+
+    # Make a horizontal bar chart of feature importances
+    plt.figure(figsize = (10, 6))
+    ax = plt.subplot()
+
+    # Need to reverse the index to plot most important on top
+    ax.barh(list(reversed(list(df.index[:15]))),
+            df['importance_normalized'].head(15),
+            align = 'center', edgecolor = 'k')
+
+    # Set the yticks and labels
+    ax.set_yticks(list(reversed(list(df.index[:15]))))
+    ax.set_yticklabels(df['feature'].head(15))
+
+    # Plot labeling
+    plt.xlabel('Normalized Importance'); plt.title('Feature Importances')
+    plt.show()
+
+    # Cumulative importance plot
+    plt.figure(figsize = (8, 6))
+    plt.plot(list(range(len(df))), df['cumulative_importance'], 'r-')
+    plt.xlabel('Number of Features'); plt.ylabel('Cumulative Importance');
+    plt.title('Cumulative Feature Importance');
+    plt.show();
+
+    importance_index = np.min(np.where(df['cumulative_importance'] > threshold))
+    print('%d features required for %0.2f of cumulative importance' % (importance_index + 1, threshold))
+
+    return df
